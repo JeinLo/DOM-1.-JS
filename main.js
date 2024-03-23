f2352126756811101de158062821328f158e5de5
01001110-01100001-01101110-01100101 <nane.akopyan.2004@mail.ru>
2024-03-23 22:37:51 +0300

    hw15

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { getComments, postComment } from "./api.js";
+import { getCommentDate } from "./date.js";
 import { renderComments } from "./render.js";
 
 const listElement = document.getElementById("list");
@@ -7,21 +8,6 @@ const textElement = document.getElementById("add-form-text");
 const buttonElement = document.getElementById("add-form-button");
 
 let comments = [];
-
-const getCommentDate = (date) => {
-    const currentDate = date ? new Date(date) : new Date();
-    let dd = currentDate.getDate();
-    if (dd < 10) dd = "0" + dd;
-    let mm = currentDate.getMonth() + 1;
-    if (mm < 10) mm = "0" + mm;
-    let yy = currentDate.getFullYear() % 100;
-    if (yy < 10) yy = "0" + yy;
-    let hour = currentDate.getHours();
-    if (hour < 10) hour = "0" + hour;
-    let minute = currentDate.getMinutes();
-    if (minute < 10) minute = "0" + minute;
-    return dd + "." + mm + "." + yy + " " + hour + ":" + minute;
-};
 
 const fetchPromiseGet = () => {
     getComments()
