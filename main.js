51f80decbe07e7204bd578eccbd8f6a3252d688e
01001110-01100001-01101110-01100101 <nane.akopyan.2004@mail.ru>
2024-03-30 13:52:15 +0300

    hw16

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 import { getComments } from "./api.js";
-import { getCommentDate } from "./date.js";
 import { renderComments } from "./render.js";
 import { renderForm } from "./renderForm.js";
-//import { format } from "date-fns";
+
 
 export let user = null
 export const setUser = (value) => {
@@ -25,13 +24,13 @@ export function renderApp () {
 
 let comments = [];
 
-const fetchPromiseGet = () => {
+export const fetchPromiseGet = () => {
     getComments()
     .then((responseData) => {
         const appComments = responseData.comments.map((comment) => {
         return {
             name: comment.author.name,
-            date: getCommentDate(comment.date),
+            date: comment.date,
             text: comment.text,
             likesQuantity: comment.likes,
             isLiked: false,
@@ -64,6 +63,7 @@ export const likeEventListener = () => {
 };
 
 export const replyEventListener = () => {
+    const textElement = document.getElementById("add-form-text");
     const commentElements = document.querySelectorAll(".comment");
     for (const commentElement of commentElements) {
         commentElement.addEventListener("click", (event) => {
--- render.js
+++ render.js
@@ -1,14 +1,16 @@
 import { postComment } from "./api.js";
-import { getCommentDate } from "./date.js";
+import { user, fetchPromiseGet } from "./main.js";
+import { format } from "date-fns";
 
 export const renderComments = ({ comments, likeEventListener, replyEventListener }) => {
     const listElement = document.getElementById("list");
     const commentsHtml = comments
     .map((comment, index) => {
+        const date = format(new Date(comment.date), "dd.MM.yyyy hh:mm");
         return `<li class="comment" data-index=${index}>
                         <div class="comment-header">
                             <div>${comment.name}</div>
-                            <div>${comment.date}</div>
+                            <div>${date}</div>
                         </div>
                         <div class="comment-body">
                             <div class="comment-text">
@@ -30,59 +32,61 @@ export const renderComments = ({ comments, likeEventListener, replyEventListener
     .join("");
     listElement.innerHTML = commentsHtml;
 
-    const nameElement = document.getElementById("add-form-name");
-    const textElement = document.getElementById("add-form-text");
-    const buttonElement = document.getElementById("add-form-button");
+    if (user) {
+        const nameElement = document.getElementById("add-form-name");
+        const textElement = document.getElementById("add-form-text");
+        const buttonElement = document.getElementById("add-form-button");
 
 
-    buttonElement.addEventListener("click", () => {
-        const date = getCommentDate();
+        buttonElement.addEventListener("click", () => {
+            nameElement.classList.remove("error");
+            textElement.classList.remove("error");
+            if (nameElement.value === "" || textElement.value === "") {
+                nameElement.classList.add("error");
+                textElement.classList.add("error");
+                return;
+            }
+            const fetchPromisePost = () => {
+                buttonElement.disabled = true;
+                buttonElement.textContent = "Комментарий публикуется...";
 
-        nameElement.classList.remove("error");
-        textElement.classList.remove("error");
-        if (nameElement.value === "" || textElement.value === "") {
-            nameElement.classList.add("error");
-            textElement.classList.add("error");
-            return;
-        }
-        const fetchPromisePost = () => {
-            buttonElement.disabled = true;
-            buttonElement.textContent = "Комментарий публикуется...";
-
-            postComment({
-                name: nameElement.value.replaceAll(">", "&gt;").replaceAll("<", "&lt;"),
-                text: textElement.value.replaceAll(">", "&gt;").replaceAll("<", "&lt;"),
-            })
-            .then(() => {
-                return fetchPromiseGet();
-            })
-            .then(() => {
-                textElement.value = "";
-                nameElement.value = "";
-                buttonElement.disabled = false;
-                buttonElement.textContent = "Написать";
-            })
-            .catch((error) => {
-                if (error.message === "Сервер сломался") {
-                    alert("Сервер сломался попробуй позже.");
+                postComment({
+                    name: nameElement.value.replaceAll(">", "&gt;").replaceAll("<", "&lt;"),
+                    text: textElement.value.replaceAll(">", "&gt;").replaceAll("<", "&lt;"),
+                })
+                .then(() => {
+                    fetchPromiseGet();
+                })
+                .then(() => {
+                    textElement.value = "";
+                    nameElement.value = "";
                     buttonElement.disabled = false;
                     buttonElement.textContent = "Написать";
-                    return;
-                }
-                if (error.message === "Плохой запрос") {
-                    alert("Ошибка в запросе, исправь данные и попробуй снова. Имя и текст должны содержать минимум 3 символа.");
-                    buttonElement.disabled = false;
-                    buttonElement.textContent = "Написать";
-                    return;
-                } else {
-                    alert("Кажется пропал интернет.");
-                }
-            });
-        };
-        fetchPromisePost();
-    });
+                })
+                .catch((error) => {
+                    if (error.message === "Сервер сломался") {
+                        alert("Сервер сломался попробуй позже.");
+                        buttonElement.disabled = false;
+                        buttonElement.textContent = "Написать";
+                        return;
+                    }
+                    if (error.message === "Плохой запрос") {
+                        alert("Ошибка в запросе, исправь данные и попробуй снова. Имя и текст должны содержать минимум 3 символа.");
+                        buttonElement.disabled = false;
+                        buttonElement.textContent = "Написать";
+                        return;
+                    } else {
+                        alert("Кажется пропал интернет.");
+                    }
+                });
+            };
+            fetchPromisePost();
+        });
+        replyEventListener();
+    }
+    
     likeEventListener();
-    replyEventListener();
+    
 };
 
 
